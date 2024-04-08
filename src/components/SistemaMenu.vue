743cf23684e98b9a713a5c49687b691abf1414ec
alvin.pino <alvin.pino@procuraduria.gob.pa>
2024-04-08 13:13:54 -0500

    Nuevos cambios

---
+++ src/components/SistemaMenu.vue
@@ -0,0 +1,12 @@
+<template>
+    <v-app>
+      pp
+      <router-view/>
+    </v-app>
+   
+</template>
+<script>
+export default {
+    name:'SistemaMenu'
+}
+</script>
---
+++ src/views/Login.vue
@@ -0,0 +1,63 @@
+<template>
+    <v-app>
+        <hello-world/>
+       <v-container>
+        <v-layout wrap>
+            <v-flex sm12 md6 offset-md3>
+                <v-card elevation="4" light tag="section">
+                    <v-card-title>
+                        <v-layout align-center justify-space-between>
+                            <h3 class="headline">
+                                {{ tituloData }}
+                            </h3>
+                        </v-layout>
+                    </v-card-title>
+                    <v-divider></v-divider>
+                    <v-card-text>
+                        <v-form>
+                            <v-text-field
+                                outlined
+                                label="Usuario"
+                                type="text"
+                                prepend-inner-icon="account_circle"
+                                autocomplete="off"
+                                placeholder="Ingrese su usuario"
+                            >
+
+                            </v-text-field>
+
+                            <v-text-field
+                            
+                            >
+
+                            </v-text-field>
+                        </v-form>
+                    </v-card-text>
+
+                </v-card>
+
+            </v-flex>
+        </v-layout>
+       </v-container>
+    </v-app>
+</template>
+<script>
+import HelloWorld from '../components/HelloWorld.vue'
+export default {
+    name:'Login',
+    components:{
+        HelloWorld
+    },
+    data() {
+        return {
+            titulo:-1
+        }
+    },
+
+    computed: {
+        tituloData(){
+            return this.titulo === -1 ? 'Sistema de Control de Insumos' : ''
+        }
+    },
+}
+</script>
